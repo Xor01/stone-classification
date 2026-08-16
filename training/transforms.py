7aac6061d04ce6b6fabd0009c70c4b3900c09752
Omar Abdullah <OmarAbdullah.q@outlook.com>
2026-08-16 14:30:29 +0300

    Update transforms.py

--- training/transforms.py
+++ training/transforms.py
@@ -4,10 +4,10 @@ def get_transforms():
     
     train_transform = T.Compose([
         T.Resize((224, 224)),
-        T.RandomHorizontalFlip(p=0.5), # تقليب أفقي
-        T.RandomVerticalFlip(p=0.5),   # تقليب رأسي (مناسب جداً للصخور)
-        T.RandomRotation(degrees=45),  # دوران للصور
-        T.ColorJitter(brightness=0.1, contrast=0.1), # تغيير خفيف للإضاءة والتباين
+        T.RandomHorizontalFlip(p=0.5), 
+        T.RandomVerticalFlip(p=0.5),   
+        T.RandomRotation(degrees=45), 
+        T.ColorJitter(brightness=0.1, contrast=0.1), 
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
